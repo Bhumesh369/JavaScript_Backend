707c058682e25b102234a293b435969070f2242c
Lakshmi Sowjanya <bhumeshwar.parate@gmail.com>
2025-12-25 22:07:55 +0530

    Comparision2

--- 01Basics/04_comparision.js
+++ 01Basics/04_comparision.js
@@ -1,1 +1,14 @@
-console.log
+console.log(2==1);
+console.log( 2!= 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0); //false
+console.log(null == 0);// false
+console.log(null>= 0); // true
+
+// === is strict check
+
+console.log("2" === 2); // false It will be true in == case
+// We shud not do conversion like all above examples , code shud be readable
