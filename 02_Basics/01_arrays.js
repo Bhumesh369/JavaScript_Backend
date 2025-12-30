e9a87c3caa8c7202538814dafc07a5a51bed9ab2
Lakshmi Sowjanya <bhumeshwar.parate@gmail.com>
2025-12-30 21:32:43 +0530

    Arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,19 @@
+// array
+
+const myArr = [0,1,2,3,4,5,6];
+myArr.push(7);
+
+console.log(myArr[1]);
+console.log(typeof myArr);
+
+// Array methods
+
+// myArr.push(8);
+// console.log(myArr);
+// myArr.pop();
+// myArr.slice(0,3)
+// myArr.splice(0,3)
+myArr.shift()
+myArr.unshift(8)
+console.log(myArr);
+
